baa0da83d4bd5fcb0ec8217665be3a98b79ff5b7
James <jwljacl@gmail.com>
2020-06-23 01:21:12 -0400

    deploy update

---
+++ build/precache-manifest.373040e6065b0cbc68adbbef62ff10c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4166c09e72e8f65f583087e5a180647",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e30e60f4101e02e795b0",
+    "url": "/static/css/main.6eb0267a.chunk.css"
+  },
+  {
+    "revision": "0ccbc5d801579abe6e74",
+    "url": "/static/js/2.920fafb0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.920fafb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e30e60f4101e02e795b0",
+    "url": "/static/js/main.25f98e7d.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "926a872e20b6555f4d2a395d6d1bc01e",
+    "url": "/static/media/Poppins-LightItalic.926a872e.ttf"
+  }
+]);
