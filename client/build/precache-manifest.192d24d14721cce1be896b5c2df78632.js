2ccf881c875109e6a4e139f663122cb912b0f3f4
James <jwljacl@gmail.com>
2020-11-24 00:25:29 -0500

    added static.json

---
+++ client/build/precache-manifest.192d24d14721cce1be896b5c2df78632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c983b2e6dfafaa936950b00345b8f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87cca0e6f520a2c5092f",
+    "url": "/static/css/main.c995e802.chunk.css"
+  },
+  {
+    "revision": "274bfc278150301eda59",
+    "url": "/static/js/2.405db2f6.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.405db2f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87cca0e6f520a2c5092f",
+    "url": "/static/js/main.f0e44c4c.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "926a872e20b6555f4d2a395d6d1bc01e",
+    "url": "/static/media/Poppins-LightItalic.926a872e.ttf"
+  }
+]);
