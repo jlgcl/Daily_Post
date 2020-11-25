61d4d73e7545c974c086c0b647b20cd1505c249a
James <jwljacl@gmail.com>
2020-11-24 21:25:26 -0500

    update

--- app.js
+++ app.js
@@ -131,7 +131,7 @@ app.use(passport.authenticate("jwt"), users);
 //   res.redirect("/");
 // });
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
